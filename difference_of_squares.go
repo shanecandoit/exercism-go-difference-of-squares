84d8965b971e12546a99f907c6df16e4286b67e8
shane <shanecandoit@gmail.com>
2019-12-30 12:26:11 -0600

    remove most comments.

--- difference_of_squares.go
+++ difference_of_squares.go
@@ -7,17 +7,9 @@ The square of the sum of the first ten natural numbers is
 The sum of the squares of the first ten natural numbers is
 1² + 2² + ... + 10² = 385.
 
-Hence the difference between the square of the sum of the first
-ten natural numbers and the sum of the squares of the first ten
-natural numbers is 3025 - 385 = 2640.
-
---- FAIL: TestSquareOfSum (0.00s)
-    difference_of_squares_test.go:14: SquareOfSum(5) = 100, want 225
---- FAIL: TestSumOfSquares (0.00s)
-    difference_of_squares_test.go:22: SumOfSquares(5) = 30, want 55
---- FAIL: TestDifference (0.00s)
-	difference_of_squares_test.go:31: Difference(5) = 70, want 170
-
+SquareOfSum(5)  = 225
+SumOfSquares(5) = 55
+Difference(5)   = 170
 */
 
 // SquareOfSum sums then square
@@ -45,16 +37,3 @@ func Difference(n int) (diff int) {
 	diff = SquareOfSum(n) - SumOfSquares(n)
 	return diff
 }
-
-/*
-func main() {
-	five := SquareOfSum(5)
-	fmt.Println("sq", five)
-
-	sum5 := SumOfSquares(5)
-	fmt.Println("sum5", sum5)
-
-	diff5 := Difference(5)
-	fmt.Println("diff5", diff5)
-}
-*/
